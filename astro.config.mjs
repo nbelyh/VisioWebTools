ea1e48bf143d35d44423623251d70751fcb6e605
Nikolay Belykh <nbelyh@gmail.com>
2023-09-03 23:49:03 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
+import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
+  site: "https://webtools.unmanagedvisio.com",
+  integrations: [react(), mdx(), sitemap()]
 });
