2e685327ec9175cd3e922b61974a637db7bb5804
Nikolay Belykh <nbelyh@gmail.com>
2023-10-24 01:55:02 +0200

    fix dotnet imports

--- src/services/useWasm.ts
+++ src/services/useWasm.ts
@@ -1,20 +1,30 @@
 import { useEffect, useState } from 'react';
 
 export const useWasm = () => {
+
+  const url = import.meta.env.DEV 
+    ? '/visiowebtools-wasm/bin/Debug/net7.0/browser-wasm/AppBundle/dotnet.js'
+    : '/AppBundle/dotnet.js';
+
   const [loading, setLoading] = useState(true);
-  const [wasm, setWasmRef] = useState<any>(null);
+  const [wasm, setWasm] = useState<any>(null);
+
+  const loadWasm = async (): Promise<any> => {
+    const module = await import(url);
+    const { getAssemblyExports, getConfig } = await module
+      .dotnet
+      .withDiagnosticTracing(false)
+      .create();
+
+    const config = getConfig();
+    const exports = await getAssemblyExports(config.mainAssemblyName);
+    return exports;
+  }
+
   useEffect(() => {
-    const visioWebToolsDotNet = (window as any)?.VisioWebToolsDotNet;
-    if (visioWebToolsDotNet) {
-      visioWebToolsDotNet.load().then((dotnet: any) => {
-          setWasmRef(dotnet)
-        }, (e: any) => {
-          setWasmRef(null);
-          console.error(e)
-        }).finally(() => {
-          setLoading(false);
-        })
-    }
+    loadWasm()
+      .then(exports => setWasm(exports))
+      .finally(() => setLoading(false))
   }, []);
   return { loading, wasm }
 }
--- visiowebtools-wasm/main.js
+++ visiowebtools-wasm/main.js
@@ -1,13 +1,1 @@
-import { dotnet } from "./dotnet.js";
-
-window.VisioWebToolsDotNet = {
-
-  load: async () => {
-    const { getAssemblyExports, getConfig } = await dotnet
-      .withDiagnosticTracing(false)
-      .create();
-  
-    const config = getConfig();
-    return await getAssemblyExports(config.mainAssemblyName);
-  }
-};
+/* an empty file to make dotnet was toolchain happy */
