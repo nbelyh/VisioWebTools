53efaf3fb00e5af5737fbd62b27cd6a72fb4f3e1
Nikolay Belykh <nbelyh@gmail.com>
2023-10-24 21:38:09 +0200

    add unit tests

--- tests/SplitFileTest.cs
+++ tests/SplitFileTest.cs
@@ -7,15 +7,35 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class SplitFileTest
 {
     [TestMethod]
-    public void RemovePage()
+    public void SplitPagesSampleFile()
     {
-        using var input = File.OpenRead(@"C:\Projects\VisioWebTools\public\samples\SplitPages.vsdx");
+        using var input = File.OpenRead(@"..\..\..\..\public\samples\SplitPages.vsdx");
+        var bytes = VisioWebTools.SplitPagesService.SplitPages(input);
 
-        using var ms = new MemoryStream();
+        using var zip = new System.IO.Compression.ZipArchive(new MemoryStream(bytes));
+        Assert.AreEqual(3, zip.Entries.Count);
+        Assert.IsTrue(zip.GetEntry("Page-1.vsdx").Length > 1000);
+    }
 
-        input.CopyTo(ms);
-        var bytes = VisioWebTools.SplitPagesService.SplitFile(ms);
+    [TestMethod]
+    public void ExtractImageSampleFile()
+    {
+        using var input = File.OpenRead(@"..\..\..\..\public\samples\ImageSample.vsdx");
+        var bytes = VisioWebTools.ExtractMediaService.ExtractMedia(input);
 
-        File.WriteAllBytes(@"C:\Projects\VisioWebTools\public\samples\Pages.zip", bytes);
+        using var zip = new System.IO.Compression.ZipArchive(new MemoryStream(bytes));
+        Assert.AreEqual(3, zip.Entries.Count);
+        Assert.IsTrue(zip.GetEntry("pageid_4_shapeid_2_image3.png").Length > 1000);
+    }
+
+    [TestMethod]
+    public void AddTooltips()
+    {
+        using var vsdx = File.OpenRead(@"..\..\..\..\public\samples\Drawing1.vsdx");
+        using var pdf = File.OpenRead(@"..\..\..\..\public\samples\Drawing1.pdf");
+        var bytes = VisioWebTools.PdfUpdater.Process(pdf, vsdx, new VisioWebTools.PdfOptions { });
+
+        Assert.IsNotNull(bytes);
+        Assert.IsTrue(bytes.Length > 1000);
     }
 }
